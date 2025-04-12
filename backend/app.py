bc032e69b04cb5161db974f28be4343eb835dd7a
RajeevG187 <rajeevg@iitbhilai.ac.in>
2025-04-12 22:47:45 +0530

    My portfolio

--- backend/app.py
+++ backend/app.py
@@ -7,8 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://portfolio-lo2x.vercel.app/"],
-    # allow_origins=["*"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
